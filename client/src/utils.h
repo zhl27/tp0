2d4f5182006be0c62ed38294486d0f61df68b7ce
zhl27 <lucazs124@gmail.com>
2024-04-19 18:10:46 +0000

    a

--- client/src/utils.h
+++ client/src/utils.h
@@ -33,6 +33,7 @@ extern t_log* logger;
 int crear_conexion(char* ip, char* puerto);
 void enviar_mensaje(char* mensaje, int socket_cliente);
 t_paquete* crear_paquete(void);
+void* serializar_paquete(t_paquete*, int);
 void agregar_a_paquete(t_paquete* paquete, void* valor, int tamanio);
 void enviar_paquete(t_paquete* paquete, int socket_cliente);
 void liberar_conexion(int socket_cliente);
